3516a5bc6417fbaf655178da047451f8707a8a70
awalley86 <andrewwalley@rocketmail.com>
2014-06-16 17:48:48 -0500

    Revisions for separation

--- week_2/bank_account.rb
+++ week_2/bank_account.rb
@@ -1,43 +1,73 @@
-module Interest
-  def calculateInterest(amount)
-    puts "#{sprintf("$%.2f", ((amount * 0.15) * 1))}"
+class WithdrawalMoreThanBalanceError < StandardError
+  def to_s
+    "The withdrawal amount cannot exceed the balance"
   end
 end
 
+class WrongDataTypeForWithdrawError < StandardError
+  def to_s
+    "The withdraw amount must be a number"
+  end
+end
+
+class WrongDataTypeForDepositError < StandardError
+  def to_s
+    "The deposit amount must be a number"
+  end
+end
+
+module Logger
+  def calculate_interest(amount)
+    sprintf("$%.2f", ((amount * 0.15) * 1))
+  end
+end
+
+
+
 class BankAccount
-  include Interest
+  include Logger
 
   attr_reader :balance
   attr_accessor :deposit_log, :name
+
+  def self.new_with_name(starting_name)
+    account = BankAccount.new
+    account.name = starting_name
+    account
+  end
 
   def to_s
     sprintf("$%.2f", self.balance)
   end
 
-  def initialize
-    @balance = 0
+  def initialize(starting_balance = 0, starting_name = 0)
+    @balance = starting_balance
+    @name = starting_name
     @deposit_log = ""
     @deposits = [] 
   end
  
   def deposit(amount)
+    raise WrongDataTypeForDepositError unless amount.is_a? Numeric
     @balance += amount
     @deposit_log = "Deposit of #{sprintf("$%.2f", amount)}"
-    @deposits.push "#{self.deposit_log}"
+    @deposits.push "#{self.deposit_log}"  
   end
 
   def history
+    log_output = "" 
     puts "History for account \"#{self.name}\""
-    @deposits.each do |log|
-      log
+    @deposits.each do |log| 
+      log_output += "#{log}\n"
     end
-
   end
 
   def withdraw(withdraw_amount)
-    @balance -= (withdraw_amount) 
-    @deposit_log = "Withdrawal of #{sprintf("$%.2f", withdraw_amount)}"
-    @deposits.push("#{self.deposit_log}")
+      raise WithdrawalMoreThanBalanceError unless withdraw_amount < self.balance    
+      raise WrongDataTypeForWithdrawError unless withdraw_amount.is_a? Numeric
+      @balance -= (withdraw_amount) 
+      @deposit_log = "Withdrawal of #{sprintf("$%.2f", withdraw_amount)}"
+      @deposits.push("#{self.deposit_log}")
   end
 
   def summary
@@ -47,8 +77,9 @@ class BankAccount
 
 end
 
-account = BankAccount.new
-account.name = "TestAccount"
+account = BankAccount.new_with_name("Homer's Savings")
+
+
 
 account.deposit 20
 account.deposit 20
@@ -57,4 +88,4 @@ account.deposit 100
 
 puts account.history
 puts account.summary
-account.calculateInterest(account.balance) 
+puts account.calculate_interest(account.balance) 
