4c831b6c43b3e8e71be9134127b5396ac4ed9a90
awalley86 <andrewwalley@rocketmail.com>
2014-07-11 23:35:21 -0500

    Adds Bundler

--- week_4/store/templates/external_templates/app.rb
+++ week_4/store/templates/external_templates/app.rb
@@ -1,18 +1,19 @@
-require 'sinatra'
+require 'sinatra/base'
 
-get "/" do
-  @title = "Home"
-  @message = "Hello World"
-  erb :index
+class LinksApp < Sinatra::Base
+  get "/" do
+    @title = "Home"
+    @message = "Hello World"
+    erb :index
+  end
+
+
+  get "/links" do 
+    @links = [
+      {:text => "Google", :href => "http://google.com"},
+      {:text => "Hacker News", :href => "http://news.ycombinator.com"},
+      {:text => "This Week In Tech", :href => "http://twit.tv/show/this-weekin-tech"}
+    ]
+    erb :links
+  end 
 end
-
-
-get "/links" do 
-  @links = [
-    {:text => "Google", :href => "http://google.com"},
-    {:text => "Hacker News", :href => "http://news.ycombinator.com"},
-    {:text => "This Week In Tech", :href => "http://twit.tv/show/this-weekin-tech"}
-  ]
-  erb :links
-end 
-
